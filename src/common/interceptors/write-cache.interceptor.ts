b3a222bf34d6558c0bc96cfd80d7fd0ddbce4a3f
Faizan Ali <fali.abdulali@gmail.com>
2025-09-25 22:51:00 +0500

    spelling fix

--- src/common/interceptors/write-cache.interceptor.ts
+++ src/common/interceptors/write-cache.interceptor.ts
@@ -1,4 +1,5 @@
 import {
+  Logger,
   Injectable,
   CallHandler,
   NestInterceptor,
@@ -11,12 +12,7 @@ import { NO_CACHE_KEY } from '../decorators/no-cache.decorator';
 
 // extra invalidations config
 const INVALIDATION_MAP: Record<string, string[]> = {
-  activity: [
-    'GET:/users/liked',
-    'GET:/users/subscriptions',
-    'GET:/recommendation/group',
-    'GET:/recommendation/single',
-  ],
+  activities: ['users/liked', 'users/subscriptions', 'recommendation'],
 };
 
 @Injectable()
@@ -47,23 +43,16 @@ export class WriteCacheInterceptor implements NestInterceptor {
     return next.handle().pipe(
       tap(async () => {
         try {
-          const keysToDelete: string[] = [];
+          const allKeys = await this.redis.keys();
+          const patterns = [prefix, ...(INVALIDATION_MAP[prefix] ?? [])];
 
-          // always invalidate the list
-          keysToDelete.push(`GET:/${prefix}`);
+          const toDelete = allKeys.filter((key) =>
+            patterns.some((p) => key.includes(p)),
+          );
 
-          // also invalidate the entity if ID present
-          if (maybeId) {
-            keysToDelete.push(`GET:/${prefix}/${maybeId}`);
+          if (toDelete.length > 0) {
+            await Promise.all(toDelete.map((k) => this.redis.del(k)));
           }
-
-          // add any configured invalidations
-          const extraInvalidations = INVALIDATION_MAP[prefix] ?? [];
-          keysToDelete.push(...extraInvalidations);
-
-          // deduplicate + delete
-          const uniqueKeys = [...new Set(keysToDelete)];
-          await Promise.all(uniqueKeys.map((k) => this.redis.del(k)));
         } catch (err) {
           console.error('Cache invalidation failed', err);
         }
